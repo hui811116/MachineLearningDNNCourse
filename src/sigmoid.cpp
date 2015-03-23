0ddf8276cd750a5adc43789b8bcce66057ea49db
Huang Teng-Hui <hui811116@outlook.com>
2015-03-23 23:37:21 +0800

    header update
    sigmoid.h

--- src/sigmoid.cpp
+++ src/sigmoid.cpp
@@ -9,43 +9,56 @@
 
 
 using namespace std;
-using namespace ext;
+//using namespace ext;
+using namespace func;
 
 typedef device_matrix<float> mat;
 typedef thrust::device_vector<float> vec;
 
 Sigmoid::Sigmoid(){
 	_weight = new mat(1,2);
-	_sigout = new mat(2,1);
-	_input = new mat(1,1);
+	_sigout.resize(2,1);
+	_input.resize(1,1);
 	_weight->fillwith(0);
 }
 Sigmoid::Sigmoid(const mat& m){
 	_weight = new mat(m);
-	_sigout = new mat(_weight->getRows(),1);
-	_input = new mat(_weight->getCols()-1,1);
+	_sigout.resize(_weight->getRows(),1);
+	_input.resize(_weight->getCols()-1,1);
 }
 Sigmoid::Sigmoid(size_t row, size_t col){
 	_weight = new mat(row,col+1);  // +1 for bias
-	_sigout = new mat(row,1);
-	_input = new mat(col,1);
+	_sigout.resize(row,1);
+	_input.resize(col,1);
 	rand_init();
 }
 Sigmoid::~Sigmoid(){
 	delete _weight;
-	delete _sigout;
-	delete _input;
 
 }
 
-void Sigmoid::forward(const mat& in, mat& out){
+void Sigmoid::forward(mat& out, const mat& in, bool train){
 	//assume in is a vector
-	out = sigmoid( *_weight * in);
+	mat* _inp = new mat(in);
+	_inp->resize(in.getRows()+1,in.getCols());
+	float* h_data=_inp->getData();
+	h_data[in.getRows()]=1;
+	//fill with 1 for computation simplicity
+	out = sigmoid( *_weight * *_inp);
+	// 
+	if(train){
+		_input = in;
+		_sigout = *_weight * *_inp;	
+	}
+	delete _inp;
 }
 
 // assume error pass through var "delta"
-Sigmoid::backPropagate(const mat& err, mat& out){
-	
+Sigmoid::backPropagate(mat& out, const mat& delta, float rate){
+	mat _tmp(~ (*_weight) * delta);
+	mat _tmp2()
+
+	delete _tmp;
 }
 
 void Sigmoid::print(ofstream& out){
