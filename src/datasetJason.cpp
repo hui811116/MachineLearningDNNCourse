90004c9f322793f307161da743dcfae7c2718574
jasonmhwu <j001001j001001@gmail.com>
2015-03-26 01:04:53 +0800

    update dataset.h

---
+++ src/datasetJason.cpp
@@ -0,0 +1,17 @@
+#include "dataset.h"
+
+mat Dataset::getBatch(int batchSize){
+
+}
+
+mat Dataset::getTrainSet(int trainSize){
+
+}
+
+mat Dataset::getValidSet(){
+
+}
+
+mat Dataset::dataSegment(){
+
+}
