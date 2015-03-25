bc7a9eff683bd26ace3308703d33900d6208b5d4
AhPan <lifeislikeamelody27@gmail.com>
2015-03-25 21:29:20 +0800

    ahpan

--- datasetTest.cpp
+++ datasetTest.cpp
@@ -6,6 +6,8 @@ int main()
 {
 	const int phonemeNum = 39;
 	const int dataNum = 180406;
-	const char* inputName = "~/larry/";	
+	const char* inputName = "~/../larry/Documents/data/MLDS_HW1_RELEASE_v1/mfcc/test.ark";	
+	Dataset test = Dataset(inputName);
+
 	return 0;
 }
