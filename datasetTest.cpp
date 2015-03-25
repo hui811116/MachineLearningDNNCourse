0cc7eee5dccc799b83f18f3aaba5a9e2602ab383
AhPan <lifeislikeamelody27@gmail.com>
2015-03-25 19:47:35 +0800

    AhPan Test Git

---
+++ datasetTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <dataset.h>
+
+int main()
+{
+	const int phonemeNum = 39;
+	const int dataNum = 180406;
+	const char* inputName = "~/larry/";	
+	return 0;
+}
